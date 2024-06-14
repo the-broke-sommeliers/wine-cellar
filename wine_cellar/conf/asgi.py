a690d9466ac0e0bd0e61e02be21892a6a0b5cf63
goapunk <djulian@riseup.net>
2024-06-14 09:02:48 +0200

    fix wrong settings path *sgi apps

--- wine_cellar/conf/asgi.py
+++ wine_cellar/conf/asgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "wine_cellar.settings")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "wine_cellar.conf.settings")
 
 application = get_asgi_application()
