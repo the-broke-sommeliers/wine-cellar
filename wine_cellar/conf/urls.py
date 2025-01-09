acf519e3843eca04d4513bcc37b849fca9de5193
Julian Dehm <j.dehm@liqd.net>
2025-01-09 18:26:59 +0100

    add option to scan barcode
    allows for quickly adding or removing bottles from stock

--- wine_cellar/conf/urls.py
+++ wine_cellar/conf/urls.py
@@ -9,9 +9,12 @@ from django.urls import include, path
 
 from wine_cellar.apps.wine.views import (
     HomePageView,
+    WineChangeStockView,
     WineCreateView,
     WineDetailView,
     WineListView,
+    WineScannedView,
+    WineScanView,
     WineUpdateView,
 )
 
@@ -19,9 +22,17 @@ urlpatterns = [
     path("admin/", admin.site.urls),
     path("accounts/", include("django.contrib.auth.urls")),
     path("wine/add/", WineCreateView.as_view(), name="wine-add"),
+    path("wine/add/<str:code>", WineCreateView.as_view(), name="wine-add"),
     path("wine/<int:pk>", WineDetailView.as_view(), name="wine-detail"),
     path("wine/edit/<int:pk>", WineUpdateView.as_view(), name="wine-edit"),
     path("wines/", WineListView.as_view(), name="wine-list"),
+    path("wine/scan", WineScanView.as_view(), name="wine-scan"),
+    path("wine/scan/<str:code>", WineScannedView.as_view(), name="wine-scan"),
+    path(
+        "wine/change_stock/<int:pk><int:op>",
+        WineChangeStockView.as_view(),
+        name="change-stock",
+    ),
     path("", HomePageView.as_view(), name="homepage"),
 ]
 
