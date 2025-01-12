4428896a4cc4a84e866cae19aa2867fcd5b38ca6
phil <5871230+philli-m@users.noreply.github.com>
2025-01-12 20:30:46 +0100

    add footer styling

--- wine_cellar/conf/settings.py
+++ wine_cellar/conf/settings.py
@@ -10,12 +10,13 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.0/ref/settings/
 """
 
-import os
 from pathlib import Path
+
+from wine_cellar import __version__
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
-
+VERSION = __version__
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
