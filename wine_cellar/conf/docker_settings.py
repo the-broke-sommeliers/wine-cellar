a92f61f873c00d672f95552d7102330e27e5d96f
Julian Dehm <j.dehm@liqd.net>
2025-02-01 14:56:57 +0100

    fix some issues with the dockerfile

--- wine_cellar/conf/docker_settings.py
+++ wine_cellar/conf/docker_settings.py
@@ -1,4 +1,16 @@
 import os
 from wine_cellar.conf.settings import *
 
+DEBUG = bool(os.environ.get("DEBUG", default=0))
 ALLOWED_HOSTS = os.environ.get("DJANGO_ALLOWED_HOSTS").split(" ")
+SECRET_KEY = os.environ.get("SECRET_KEY")
+DATABASES = {
+    "default": {
+        "ENGINE": os.environ.get("SQL_ENGINE", "django.db.backends.sqlite3"),
+        "NAME": os.environ.get("SQL_DATABASE", BASE_DIR / "db.sqlite3"),
+        "USER": os.environ.get("SQL_USER", "user"),
+        "PASSWORD": os.environ.get("SQL_PASSWORD", "password"),
+        "HOST": os.environ.get("SQL_HOST", "localhost"),
+        "PORT": os.environ.get("SQL_PORT", "5432"),
+    }
+}
