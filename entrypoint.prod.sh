3e09ad9e33297a71c4c1b932ab2b57ca76777f98
Julian Dehm <j.dehm@liqd.net>
2025-02-01 15:09:46 +0100

    add initial docker-prod config

--- entrypoint.prod.sh
+++ entrypoint.prod.sh
@@ -11,8 +11,8 @@ then
     echo "PostgreSQL started"
 fi
 
-python manage.py flush --no-input
 python manage.py migrate
+python manage.py collectstatic --no-input --clear
 
 cat <<EOF | python manage.py shell
 from django.contrib.auth import get_user_model
