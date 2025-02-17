48931c971aa4f976a8249008bbcf244656f02bef
Julian Dehm <j.dehm@liqd.net>
2025-02-17 22:17:51 +0100

    add sorting options to wine list

--- wine_cellar/apps/wine/filters.py
+++ wine_cellar/apps/wine/filters.py
@@ -1,11 +1,27 @@
 import django_filters
 from django.db.models import Q
+from django.utils.translation import gettext as _
+from django_filters import OrderingFilter
 
 from wine_cellar.apps.wine.forms import WineFilterForm
 from wine_cellar.apps.wine.models import Wine
 
 
 class WineFilter(django_filters.FilterSet):
+    order = OrderingFilter(
+        choices=(
+            ("-created", _("Recently Added")),
+            ("created", _("Least Recently Added")),
+            ("-name", _("Name Descending")),
+            ("name", _("Name Ascending")),
+            ("-vintage", _("Newest First")),
+            ("vintage", _("Oldest First")),
+        ),
+        label=_("Sorting"),
+        empty_label=None,
+        null_label=None,
+    )
+
     class Meta:
         form = WineFilterForm
         model = Wine
