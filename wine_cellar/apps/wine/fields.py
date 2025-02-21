a163d00cba2c6c712d20151a64c82ca03e1e0301
Julian Dehm <j.dehm@liqd.net>
2025-02-21 21:32:53 +0100

    add tests for fields and remove some redundant code

--- wine_cellar/apps/wine/fields.py
+++ wine_cellar/apps/wine/fields.py
@@ -1,5 +1,6 @@
 from django.core.exceptions import ValidationError
 from django.forms import ModelMultipleChoiceField
+from django.utils.translation import gettext_lazy as _
 
 
 class OpenMultipleChoiceField(ModelMultipleChoiceField):
@@ -15,14 +16,16 @@ class OpenMultipleChoiceField(ModelMultipleChoiceField):
 
     def __init__(self, queryset, field_name, field_class=None, **kwargs):
         super().__init__(queryset, **kwargs)
+        self.default_error_messages.update(
+            {
+                "invalid_new_value": _("“%(pk)s” is not a valid value."),
+            }
+        )
+
         self.field_name = field_name
         self.field_class = field_class if field_class else None
 
-    def validate(self, value):
-        if self.required and not value:
-            raise ValidationError(self.error_messages["required"], code="required")
-
-    # code taken from ModelMultipleChoiceField with addition of creating a
+    # code taken from ModelMultipleChoiceField with addition of creating new choices
     def _check_values(self, value):
         """
         Given a list of possible PK values, return a QuerySet of the
@@ -51,7 +54,14 @@ class OpenMultipleChoiceField(ModelMultipleChoiceField):
                 if isinstance(pk, str) and pk.startswith("tom_new_opt"):
                     v = pk.removeprefix("tom_new_opt")
                     if self.field_class:
-                        v = self.field_class(v)
+                        try:
+                            v = self.field_class(v)
+                        except ValueError:
+                            raise ValidationError(
+                                self.error_messages["invalid_new_value"],
+                                code="invalid_new_value",
+                                params={"pk": pk},
+                            )
                     new_value, _ = self.queryset.get_or_create(
                         **{self.field_name: v, "user": self.user}
                     )
