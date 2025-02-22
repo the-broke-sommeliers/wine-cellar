816896eeb558d50fc1b05553e2331eab24da8cd0
Julian Dehm <j.dehm@liqd.net>
2025-02-22 10:19:54 +0100

    add more tests for wine form, scan views, change stock views

--- tests/test_views.py
+++ tests/test_views.py
@@ -30,6 +30,24 @@ def test_homepage(client, user):
 
 
 @pytest.mark.django_db
+def test_homepage_stats(client, user, wine_factory):
+    wine_factory(user=user, stock=1, vintage=2020)
+    wine_factory(user=user, country="DE", vintage=2023)
+    wine_factory(user=user, country="DE", vintage=2024)
+    client.force_login(user)
+    r = client.get(reverse("homepage"), follow=True)
+    assert r.status_code == HTTPStatus.OK
+    assertTemplateUsed(response=r, template_name="base.html")
+    assertTemplateUsed(response=r, template_name="homepage.html")
+    assertTemplateNotUsed(response=r, template_name="registration/login.html")
+    assert r.context_data["oldest"] == 2020
+    assert r.context_data["youngest"] == 2024
+    assert r.context_data["wines_in_stock"] == 1
+    assert r.context_data["wines"] == 3
+    assert r.context_data["countries"] == 2
+
+
+@pytest.mark.django_db
 def test_wine_create_unauthenticated(client, user):
     r = client.get(reverse("wine-add"), follow=True)
     assert r.status_code == HTTPStatus.OK
@@ -87,6 +105,145 @@ def test_wine_create_post_unauthenticated(client):
     )
     assertTemplateUsed(response=r, template_name="base.html")
     assertTemplateUsed(response=r, template_name="registration/login.html")
+    assert not Wine.objects.exists()
+
+
+@pytest.mark.django_db
+def test_wine_create_post_with_barcode(client, user):
+    client.force_login(user)
+    size = Size.objects.get(name=0.75)
+    data = {
+        "name": "Merlot",
+        "wine_type": "RE",
+        "category": "DR",
+        "abv": 13.0,
+        "size": size.pk,
+        "vintage": 2002,
+        "country": "DE",
+        "form_step": 4,
+    }
+    assert not Wine.objects.exists()
+    r = client.get(reverse("wine-add", kwargs={"code": 12345}))
+    initial = r.context_data["form"].initial.copy()
+    initial.update(data)
+    r = client.post(
+        reverse("wine-add", kwargs={"code": 12345}), data=initial, follow=True
+    )
+    assert r.status_code == HTTPStatus.OK
+    assertRedirects(response=r, expected_url=reverse("wine-list"))
+    assertTemplateUsed(response=r, template_name="base.html")
+    assertTemplateUsed(response=r, template_name="wine_list.html")
+    assert Wine.objects.exists()
+    wine = Wine.objects.first()
+    assert wine.name == data["name"]
+    assert wine.wine_type == data["wine_type"]
+    assert wine.abv == data["abv"]
+    assert wine.size == size
+    assert wine.vintage == data["vintage"]
+    assert wine.barcode == "12345"
+
+
+@pytest.mark.django_db
+def test_wine_create_post_with_steps(client, user):
+    client.force_login(user)
+    size = Size.objects.get(name=0.75)
+    data_step0 = {
+        "name": "Merlot",
+        "wine_type": "RE",
+        "size": size.pk,
+        "country": "DE",
+    }
+    assert not Wine.objects.exists()
+    r = client.get(reverse("wine-add"))
+    initial = r.context_data["form"].initial.copy()
+    assert initial["form_step"] == 0
+    initial.update(data_step0)
+    # post form step 1
+    r = client.post(reverse("wine-add"), data=initial, follow=True)
+    assert r.status_code == HTTPStatus.OK
+    assert not Wine.objects.exists()
+
+    # post form step 1
+    data_step1 = {
+        "category": "DR",
+        "abv": 13.0,
+        "vintage": 2002,
+    }
+    initial = r.context_data["form"].data.copy()
+    assert initial["form_step"] == 1
+    initial.update(data_step1)
+    r = client.post(reverse("wine-add"), data=initial, follow=True)
+    assert r.status_code == HTTPStatus.OK
+    assert not Wine.objects.exists()
+
+    # post form step 2
+    data_step2 = {
+        "source": "tom_new_optSupermarket",
+        "stock": 1,
+    }
+    initial = r.context_data["form"].data.copy()
+    assert initial["form_step"] == 2
+    initial.update(data_step2)
+    r = client.post(reverse("wine-add"), data=initial, follow=True)
+    assert r.status_code == HTTPStatus.OK
+    assert not Wine.objects.exists()
+
+    # post form step 3
+    data_step3 = {
+        "rating": 5,
+        "comment": "Good wine",
+    }
+    initial = r.context_data["form"].data.copy()
+    assert initial["form_step"] == 3
+    initial.update(data_step3)
+    r = client.post(reverse("wine-add"), data=initial, follow=True)
+    assert r.status_code == HTTPStatus.OK
+    assert not Wine.objects.exists()
+
+    # post form step 4
+    initial = r.context_data["form"].data.copy()
+    assert initial["form_step"] == 4
+    r = client.post(reverse("wine-add"), data=initial, follow=True)
+    assert r.status_code == HTTPStatus.OK
+    assertRedirects(response=r, expected_url=reverse("wine-list"))
+    assertTemplateUsed(response=r, template_name="base.html")
+    assertTemplateUsed(response=r, template_name="wine_list.html")
+    assert Wine.objects.exists()
+    wine = Wine.objects.first()
+    data = {}
+    data.update(data_step0)
+    data.update(data_step1)
+    data.update(data_step2)
+    data.update(data_step3)
+    assert wine.name == data["name"]
+    assert wine.wine_type == data["wine_type"]
+    assert wine.abv == data["abv"]
+    assert wine.size == size
+    assert wine.vintage == data["vintage"]
+    assert wine.comment == data["comment"]
+    assert wine.rating == data["rating"]
+
+
+@pytest.mark.django_db
+def test_wine_create_post_invalid_step(client, user):
+    client.force_login(user)
+    size = Size.objects.get(name=0.75)
+    data = {
+        "name": "Merlot",
+        "wine_type": "RE",
+        "category": "DR",
+        "abv": 13.0,
+        "size": size.pk,
+        "vintage": 2002,
+        "country": "DE",
+        "form_step": 5,
+    }
+    assert not Wine.objects.exists()
+    r = client.post(reverse("wine-add"), data, follow=True)
+    assert r.status_code == HTTPStatus.OK
+    assertTemplateUsed(response=r, template_name="base.html")
+    assertTemplateUsed(response=r, template_name="wine_create.html")
+    assert r.context_data["form"].errors
     assert not Wine.objects.exists()
 
 
@@ -400,3 +557,61 @@ def test_wine_update_valid_fields(
     assert changed_wine.vineyard.first() == vineyard
     assert changed_wine.source.count() == 1
     assert changed_wine.source.first() == source
+
+
+@pytest.mark.django_db
+def test_wine_scanned_existing(
+    client,
+    user,
+    wine_factory,
+):
+    wine = wine_factory(user=user, country="DE", barcode="12345")
+    client.force_login(user)
+    r = client.get(reverse("wine-scan", kwargs={"code": wine.barcode}), follow=True)
+    assert r.status_code == HTTPStatus.OK
+    assertRedirects(
+        response=r, expected_url=reverse("wine-detail", kwargs={"pk": wine.pk})
+    )
+    assertTemplateUsed(response=r, template_name="base.html")
+    assertTemplateUsed(response=r, template_name="wine_detail.html")
+
+
+@pytest.mark.django_db
+def test_wine_scanned_non_existing(
+    client,
+    user,
+    wine_factory,
+):
+    wine_factory(user=user, country="DE", barcode="12345")
+    client.force_login(user)
+    r = client.get(reverse("wine-scan", kwargs={"code": "00000"}), follow=True)
+    assert r.status_code == HTTPStatus.OK
+    assertTemplateUsed(response=r, template_name="base.html")
+    assertTemplateUsed(response=r, template_name="scanned_wine.html")
+
+
+@pytest.mark.django_db
+def test_wine_increase_stock(
+    client,
+    user,
+    wine_factory,
+):
+    wine = wine_factory(user=user, stock=0, country="DE")
+    assert wine.stock == 0
+    client.force_login(user)
+    r = client.get(
+        reverse("change-stock", kwargs={"pk": wine.pk, "op": 1}), follow=True
+    )
+    assert r.status_code == HTTPStatus.OK
+    assertTemplateUsed(response=r, template_name="base.html")
+    assertTemplateUsed(response=r, template_name="wine_detail.html")
+    wine.refresh_from_db()
+    assert wine.stock == 1
+    r = client.get(
+        reverse("change-stock", kwargs={"pk": wine.pk, "op": 0}), follow=True
+    )
+    assert r.status_code == HTTPStatus.OK
+    assertTemplateUsed(response=r, template_name="base.html")
+    assertTemplateUsed(response=r, template_name="wine_detail.html")
+    wine.refresh_from_db()
+    assert wine.stock == 0
