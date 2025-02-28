89282faff552f3ebde4758295b594b6d66363e15
Julian Dehm <j.dehm@liqd.net>
2025-02-28 12:52:15 +0100

    add missing webpack.prod.js and update dockerignore

---
+++ webpack.prod.js
@@ -0,0 +1,18 @@
+const common = require('./webpack.common.js')
+const { merge } = require('webpack-merge')
+const TerserPlugin = require('terser-webpack-plugin')
+
+module.exports = merge(common, {
+  devtool: false,
+  optimization: {
+    minimize: true,
+    minimizer: [
+      new TerserPlugin({
+        parallel: true,
+        terserOptions: {
+          ecma: 5
+        }
+      })
+    ]
+  }
+})
