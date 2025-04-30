a6720685a1170fc411f25ed04359212aea36bcd6
goapunk <julian@dehm.dev>
2025-04-30 12:53:32 +0200

    admin: add user field to user created content

--- wine_cellar/apps/wine/admin.py
+++ wine_cellar/apps/wine/admin.py
@@ -12,35 +12,35 @@ from wine_cellar.apps.wine.models import (
 
 @admin.register(Wine)
 class WineAdmin(admin.ModelAdmin):
-    list_display = ["name", "barcode"]
-    fields = ["name", "barcode"]
+    list_display = ["name", "barcode", "user"]
+    fields = ["name", "barcode", "user"]
 
 
 @admin.register(Size)
 class SizeAdmin(admin.ModelAdmin):
-    list_display = ["name"]
-    fields = ["name"]
+    list_display = ["name", "user"]
+    fields = ["name", "user"]
 
 
 @admin.register(Grape)
 class GrapeAdmin(admin.ModelAdmin):
-    list_display = ["name"]
-    fields = ["name"]
+    list_display = ["name", "user"]
+    fields = ["name", "user"]
 
 
 @admin.register(Vineyard)
 class VineyardAdmin(admin.ModelAdmin):
-    list_display = ["name", "country"]
-    fields = ["name", "website", "country", "region"]
+    list_display = ["name", "country", "user"]
+    fields = ["name", "website", "country", "region", "user"]
 
 
 @admin.register(FoodPairing)
 class FoodPairingAdmin(admin.ModelAdmin):
-    list_display = ["name"]
-    fields = ["name"]
+    list_display = ["name", "user"]
+    fields = ["name", "user"]
 
 
 @admin.register(Source)
 class SourceAdmin(admin.ModelAdmin):
-    list_display = ["name"]
-    fields = ["name"]
+    list_display = ["name", "user"]
+    fields = ["name", "user"]
