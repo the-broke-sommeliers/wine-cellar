1b89cf68269454a61f82e2a674a7ca52cfb02d79
phil <5871230+philli-m@users.noreply.github.com>
2025-08-13 13:05:28 +0200

    rm image.css file

--- webpack.common.js
+++ webpack.common.js
@@ -14,7 +14,6 @@ module.exports = {
         './wine_cellar/assets/css/styles.css',
         './wine_cellar/assets/css/page-layout.css',
         './wine_cellar/assets/css/homepage.css',
-        './wine_cellar/assets/css/image.css',
         './wine_cellar/assets/css/button.css',
         './node_modules/tom-select/dist/css/tom-select.css',
         './node_modules/@fortawesome/fontawesome-free/css/fontawesome.css',
