4144695fc8b0d55302d2e10fda990ae27230eafc
goapunk <julian@dehm.dev>
2026-01-05 18:49:38 +0100

    feat(stats): if a stock item has no price, use the price of the wine

--- tests/test_views.py
+++ tests/test_views.py
@@ -35,13 +35,14 @@ def test_homepage(client, user):
 def test_homepage_stats(client, user, wine_factory, storage_item_factory):
     wine = wine_factory(user=user, vintage=2020)
     storage = user.storage_set.first()
-    wine_2 = wine_factory(user=user, country="DE", vintage=2023)
+    wine_2 = wine_factory(user=user, country="DE", vintage=2023, price=15.00)
     wine_factory(user=user, country="ES", vintage=2024)
     storage_item_factory(wine=wine, storage=storage, price=10.50)
     storage_item_factory(wine=wine, storage=storage, price=5.25)
     storage_item_factory(wine=wine, storage=storage, price=8.99, deleted=True)
     storage_item_factory(wine=wine_2, storage=storage, price=4.99, deleted=True)
     storage_item_factory(wine=wine_2, storage=storage, price=12.00)
+    storage_item_factory(wine=wine_2, storage=storage)
     client.force_login(user)
     r = client.get(reverse("homepage"), follow=True)
     assert r.status_code == HTTPStatus.OK
@@ -54,7 +55,7 @@ def test_homepage_stats(client, user, wine_factory, storage_item_factory):
     assert r.context_data["wines_in_stock"] == 2
     assert r.context_data["wines"] == 3
     assert r.context_data["countries"] == 2
-    assert r.context_data["total_value"] == "28€"
+    assert r.context_data["total_value"] == "43€"
 
 
 @pytest.mark.django_db
