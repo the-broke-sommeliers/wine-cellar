278a6b3b1e1676c0221f2162c51b11be15574551
Julian Dehm <julian@dehm.dev>
2026-04-13 22:32:38 +0200

    feat: add option to scan barcode within add/edit form

--- wine_cellar/react/react_bar_code.tsx
+++ wine_cellar/react/react_bar_code.tsx
@@ -11,54 +11,77 @@ const translated = {
   helptext: django.gettext(
     "Choose the type of barcode you want to scan, sometimes this can help if scanning doesn't work."
   ),
+  scan_barcode: django.gettext('Scan Barcode'),
+  close_barcode: django.gettext('Close Scanner')
 }
 
-const Scanner = () => {
+const Scanner = ({ targetInputId }: { targetInputId?: string }) => {
+  const [isOpen, setIsOpen] = useState(false)
   const [selectedFormat, setSelectedFormat] = useState('any')
   const defaultFormats = ['ean_13', 'ean_8', 'upc_a', 'code_39', 'itf']
 
-  const handleCapture = (barcodes:  DetectedBarcode[]) => {
+  const handleCapture = (barcodes: DetectedBarcode[]) => {
     if (barcodes.length > 0) {
-      window.location.href = '/wine/scan/' + barcodes[0].rawValue
+      const code = barcodes[0].rawValue
+
+      if (targetInputId) {
+        const input = document.getElementById(targetInputId) as HTMLInputElement
+        if (input) {
+          input.value = code;
+          setIsOpen(false)
+        }
+      } else {
+        window.location.href = '/wine/scan/' + code
+      }
     }
   }
-
   return (
     <>
-      <section className="form__scanner__details">
-        <details>
-          <summary>{translated.advanced}</summary>
-          <p className="form-hint">{translated.helptext}</p>
-          <select
-            value={selectedFormat}
-            onChange={(e) => setSelectedFormat(e.target.value)}
-          >
-            <option value="">{django.gettext('All')}</option>
-            <option value="code_39">Code 39</option>
-            <option value="code_93">Code 93</option>
-            <option value="code_128">Code 128</option>
-            <option value="ean_8">EAN-8</option>
-            <option value="ean_13">EAN-13</option>
-            <option value="itf">ITF</option>
-            <option value="upc_a">UPC-A</option>
-            <option value="upc_e">UPC-E</option>
-          </select>
-        </details>
-      </section>
-      <section className="form__scanner">
-        <BarcodeScanner
-          onCapture={handleCapture}
-          options={{
-            formats: selectedFormat ? [selectedFormat] : defaultFormats,
-          }}
-        />
-        <div className="overlay">
-          <div className="overlay-element top-left" />
-          <div className="overlay-element top-right" />
-          <div className="overlay-element bottom-left" />
-          <div className="overlay-element bottom-right" />
-        </div>
-      </section>
+      <button
+        type="button"
+        className="pure-button button__secondary form__scanner__button"
+        onClick={() => setIsOpen(!isOpen)}
+      >
+        {isOpen ? translated.close_barcode : translated.scan_barcode}
+      </button>
+      {isOpen && (
+        <>
+          <section className="form__scanner__details">
+            <details>
+              <summary>{translated.advanced}</summary>
+              <p className="form-hint">{translated.helptext}</p>
+              <select
+                value={selectedFormat}
+                onChange={(e) => setSelectedFormat(e.target.value)}
+              >
+                <option value="">{django.gettext('All')}</option>
+                <option value="code_39">Code 39</option>
+                <option value="code_93">Code 93</option>
+                <option value="code_128">Code 128</option>
+                <option value="ean_8">EAN-8</option>
+                <option value="ean_13">EAN-13</option>
+                <option value="itf">ITF</option>
+                <option value="upc_a">UPC-A</option>
+                <option value="upc_e">UPC-E</option>
+              </select>
+            </details>
+          </section>
+          <section className="form__scanner">
+            <BarcodeScanner
+              onCapture={handleCapture}
+              options={{
+                formats: selectedFormat ? [selectedFormat] : defaultFormats,
+              }}
+            />
+            <div className="overlay">
+              <div className="overlay-element top-left" />
+              <div className="overlay-element top-right" />
+              <div className="overlay-element bottom-left" />
+              <div className="overlay-element bottom-right" />
+            </div>
+          </section>
+        </>
+      )}
     </>
   )
 }
@@ -67,6 +90,7 @@ const initScanner = () => {
   const container = document.getElementById('scanner')
   if (container) {
     const root = createRoot(container)
+    const targetInputId = container.dataset.targetInput
     // Override the locateFile function
     prepareZXingModule({
       overrides: {
@@ -81,7 +105,7 @@ const initScanner = () => {
     })
     // @ts-ignore
     globalThis.BarcodeDetector ??= BarcodeDetector
-    root.render(<Scanner />)
+    root.render(<Scanner targetInputId={targetInputId} />)
   }
 }
 
