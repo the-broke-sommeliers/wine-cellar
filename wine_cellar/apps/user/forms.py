09f2b6271d34ac2eaa0899d42b052c1d9c9aa5bb
Julian Dehm <julian@dehm.dev>
2026-05-14 12:06:39 +0200

    apps/users: make settings menu headings translatable

--- wine_cellar/apps/user/forms.py
+++ wine_cellar/apps/user/forms.py
@@ -9,6 +9,11 @@ class UserSettingsForm(ModelForm):
     class Meta:
         model = UserSettings
         fields = ["language", "currency", "notifications"]
+        labels = {
+            "language": _("Language"),
+            "currency": _("Currency"),
+            "notifications": _("Email Notifications"),
+        }
         help_texts = {
             "language": _("The language the site is displayed in."),
             "currency": _("The default currency used for the price of a wine."),
