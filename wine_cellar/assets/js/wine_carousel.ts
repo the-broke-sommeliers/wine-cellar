233b280abbb31c8fafbcebfe9f408b63208bea7e
goapunk <julian@dehm.dev>
2026-05-17 11:07:55 +0200

    feat: make wine images clickable and open in new tab

--- wine_cellar/assets/js/wine_carousel.ts
+++ wine_cellar/assets/js/wine_carousel.ts
@@ -1,4 +1,4 @@
-document.addEventListener('DOMContentLoaded', () => {
+function initWineCarousel() {
   const wrapper = document.querySelector(
     '.wine-detail__image-wrapper'
   ) as HTMLElement
@@ -7,14 +7,30 @@ document.addEventListener('DOMContentLoaded', () => {
   }
 
   const images = JSON.parse(wrapper.dataset.images || '')
+
+  if (!Array.isArray(images) || images.length < 2) {
+    return
+  }
+
   let index = 0
 
   const imgEl = document.getElementById('wine-image') as HTMLImageElement
+  const anchor = wrapper.querySelector(
+    '.image-fullscreen-anchor'
+  ) as HTMLAnchorElement
   const prevBtn = wrapper.querySelector('.wine-prev') as HTMLButtonElement
   const nextBtn = wrapper.querySelector('.wine-next') as HTMLButtonElement
 
+  if (!imgEl || !prevBtn || !nextBtn) {
+    console.error('Carousel elements not found')
+    return
+  }
+
   function updateImage() {
     imgEl.src = images[index]
+    anchor.href = images[index].replace('_thumb', '')
+    prevBtn.disabled = index === 0
+    nextBtn.disabled = index === images.length - 1
   }
 
   prevBtn.addEventListener('click', () => {
@@ -26,4 +42,6 @@ document.addEventListener('DOMContentLoaded', () => {
     index = (index + 1) % images.length
     updateImage()
   })
-})
+}
+
+document.addEventListener('DOMContentLoaded', initWineCarousel)
